af1d1f3a94d7ea6026a46d30a0b06701fca4c019
Juliana <112645443+Vlrrn@users.noreply.github.com>
2023-10-09 13:29:55 +0300

    Add files via upload

--- 3.2.py
+++ 3.2.py
@@ -6,20 +6,52 @@
     # Пример файла:
     # Машинка 50
     # Кукла 40
+import os
+
 print("\033[3m\033[36m{}{}{}\033[0m".format("\n", " " * 8, "Задание 2\n"))
-toys = {}
-with open("File_3_2.txt", encoding="utf-8") as file_3_2:
-    for line in file_3_2.readlines():
-        name, amount = line.split(' ')
-        toys[name] = amount.strip()
-    min_v = int(toys[list(toys.keys())[0]])
-    for key, value in toys.items():
-        if int(value) < 10:
-            print(key, ' - ', value)
-        if int(value) < min_v:
-            min_v = int(value)
-    print("\nМинимальная стоимость:", min_v)
-    print()
-    for key, value in toys.items():
-        if int(value) == min_v:
-            print(key, ' - ', value)
+while True:
+    if os.path.exists("File_3_2.txt"):
+        toys = {}
+        with open("File_3_2.txt", encoding="utf-8") as file_3_2:
+            print("Данные из файла:")
+            print(file_3_2.read(), '\n')
+            file_3_2.seek(0)
+            print("Стоимость ниже 10р:")
+            for line in file_3_2.readlines():
+                # print(line)
+                name, amount = line.split(' ')
+                # print(name, amount)
+                toys[name] = amount.strip()
+            # print(toys)
+            min_v = int(toys[list(toys.keys())[0]])
+            # print(min_v)
+            for key, value in toys.items():
+                if int(value) < 10:
+                    print(key, ' - ', value)
+                if int(value) < min_v:
+                    min_v = int(value)
+            print("\nМинимальная стоимость:", min_v)
+            print()
+            # print(min_v, type(min_v))
+            for key, value in toys.items():
+                if int(value) == min_v:
+                    print(key, ' - ', value)
+        break
+    else:
+        text = (
+"""Машинка 50
+Кукла 40
+Раскраска 5
+Пазл 8
+Конструктор 45
+Медведь 50
+Карандаши 5
+Сказки 9
+Робот 50
+Глобус 25
+Поезд 30
+Наклейки 5"""
+        )
+        # print("Нет файла.")
+        with open("File_3_2.txt",'w', encoding="utf-8") as file_3_2:
+            file_3_2.write(text)
--- 3.4.py
+++ 3.4.py
@@ -10,46 +10,63 @@
 # Итоговый список сохранить в виде json-объекта в соответствующий файл.
 #   Пример json-объекта: [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 import json
+import os
 
 print("\033[3m\033[36m{}{}{}\033[0m".format("\n", " " * 8, "Задание 4\n"))
-with (open("File_3_4.txt", encoding="utf-8") as file_3_4):
-    print(" " * 2, "Данные о фирмах:")
-    print(file_3_4.read(), '\n')
-    file_3_4.seek(0)
-    s = {}
-    s2 = {}
-    list_ = []
-    for line in file_3_4.readlines():
-        st = line.split(' ')
-        for el in st:
-            i = st.index(el)
-            el = el.replace("\n", '')
-            st[i] = el
-        del st[1]
-        money = int(st[1]) - int(st[2])
-        name = st[0]
-        s[name] = money
-    # for key, value in s.items():
-    #     print(key, ':', value)
-    # print(s)
-    n = 0
-    c = 0
-    for value in s.values():
-        if int(value) > 0:
-            n += 1
-            c += int(value)
-    k = "average_profit"
-    v = c / n
-    s2[k] = int(v)
-    # print(s2)
-    list_.append(s)
-    list_.append(s2)
-    print(" " * 2, "Словарь:")
-    for e in list_:
-        for key, value in e.items():
-            print(key, ':', value)
-    # print(list_)
-print()
-with open("File.json", 'w') as file_json:
-    json.dump(list_, file_json)
-    print("json-объект:", json.dumps(list_))
+while True:
+    if os.path.exists("File_3_4.txt"):
+        with open("File_3_4.txt", encoding="utf-8") as file_3_4:
+            print(" " * 2, "Данные о фирмах:")
+            print(file_3_4.read(), '\n')
+            file_3_4.seek(0)
+            s = {}
+            s2 = {}
+            list_ = []
+            for line in file_3_4.readlines():
+                st = line.split(' ')
+                for el in st:
+                    i = st.index(el)
+                    el = el.replace("\n", '')
+                    st[i] = el
+                del st[1]
+                money = int(st[1]) - int(st[2])
+                name = st[0]
+                s[name] = money
+            # for key, value in s.items():
+            #     print(key, ':', value)
+            # print(s)
+            n = 0
+            c = 0
+            for value in s.values():
+                if int(value) > 0:
+                    n += 1
+                    c += int(value)
+            k = "average_profit"
+            v = c / n
+            s2[k] = int(v)
+            # print(s2)
+            list_.append(s)
+            list_.append(s2)
+            print(" " * 2, "Словарь:")
+            for e in list_:
+                for key, value in e.items():
+                    print(key, ':', value)
+            # print(list_)
+        print()
+        with open("File.json", 'w') as file_json:
+            json.dump(list_, file_json)
+            print("json-объект:", json.dumps(list_))
+        break
+    else:
+        text = (
+"""firm_1 ООО 10000 5000
+firm_2 ООО 1000 5000
+firm_3 ООО 10000 4000
+firm_4 ООО 15000 5000
+firm_5 ООО 10000 5000
+firm_6 ООО 10000 2000
+firm_7 ООО 7000 5000"""
+        )
+        # print("---")
+        with open("File_3_4.txt", 'w', encoding='utf-8') as file_3_4:
+            file_3_4.write(text)
